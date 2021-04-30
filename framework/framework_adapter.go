972212fcfd430318e3a158abb6a1e2c929b91af5
pagongamedev <pagongamedev@gmail.com>
2021-05-01 01:56:45 +0700

    Update Go Adapter

--- framework/framework_adapter.go
+++ framework/framework_adapter.go
@@ -5,14 +5,14 @@ import godd "github.com/pagongamedev/go-dd"
 // AdapterApp Func
 func AdapterApp(app interface{}, frameworkType ...godd.FrameWork) godd.InterfaceApp {
 
-	fwType := godd.FrameWorkGofiber
+	fwType := godd.FrameWorkGofiberV2
 	if len(frameworkType) > 0 {
 		fwType = frameworkType[0]
 	}
 	var interfaceApp godd.InterfaceApp
 
 	switch fwType {
-	case godd.FrameWorkGofiber:
+	case godd.FrameWorkGofiberV2:
 		interfaceApp = AdapterAppGofiber(app, fwType)
 		break
 	}
